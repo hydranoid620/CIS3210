af25d4b95cb1bfb34c0b41ca06eab2e56c6852ed
Nicholas Rosati <nrosati@uoguelph.ca>
2020-09-15 20:36:09 -0400

    Add more functionality and a little styling

--- static/js/init.js
+++ static/js/init.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
+    //Sets animations for the buttons
+    clippy.load('Links', function(agent) {
+        agent.show();
 
-    // Load Links
-	
-	//Replace with a Links animation
-    $('#alert-button').click(function () {
-		alert("Alert!");
+        $("#greeting-button").on("click", function() {agent.play("Greeting")});
+        $("#artsy-button").on("click", function() {agent.play("GetArtsy")});
+        $("#alert-button").on("click", function() {agent.play("Alert")});
+
+        $("#wave-button").on("click", function() {agent.play("Wave")});
+        $("#save-button").on("click", function() {agent.play("Save")});
+        $("#print-button").on("click", function() {agent.play("Print")});
+
+        $("#get-wizardy-button").on("click", function() {agent.play("Get Wizardy")});
+        $("#congratulate-button").on("click", function() {agent.play("Congratulate")});
+        $("#empty-trash-button").on("click", function() {agent.play("Empty Trash")});
+
+        $("#stop-current-button").on("click", function() {agent.stopCurrent()});
+        $("#stop-all-button").on("click", function() {agent.stop()});
     });
-        
 });
