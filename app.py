c84b11892123172e58d709912f299e8af7f282ae
Nicholas Rosati <nrosati@uoguelph.ca>
2020-09-23 00:25:09 -0400

    Removed Links

--- app.py
+++ app.py
@@ -9,3 +9,5 @@ app = Flask(__name__, static_url_path='')
 @app.route('/')
 def index(name=None):
     return render_template('index.html', name=name)
+
+
--- static/js/init.js
+++ static/js/init.js
@@ -1,24 +1,4 @@
 $(document).ready(function () {
-    //Sets animations for the buttons
-    clippy.load('Links', function(agent) {
-        agent.show();
 
-        $("#greeting-button").on("click", function() {agent.play("Greeting")});
-        $("#artsy-button").on("click", function() {agent.play("GetArtsy")});
-        $("#alert-button").on("click", function() {agent.play("Alert")});
-
-        $("#wave-button").on("click", function() {agent.play("Wave")});
-        $("#save-button").on("click", function() {agent.play("Save")});
-        $("#print-button").on("click", function() {agent.play("Print")});
-
-        $("#get-wizardy-button").on("click", function() {agent.play("GetWizardy")});
-        $("#congratulate-button").on("click", function() {agent.play("Congratulate")});
-        $("#empty-trash-button").on("click", function() {agent.play("EmptyTrash")});
-
-        $("#stop-current-button").on("click", function() {agent.stopCurrent()});
-        $("#stop-all-button").on("click", function() {agent.stop()});
-
-
-        console.log("Name: Nicholas Rosati\nStudent Number: 1037025");
-    });
+    console.log("Name: Nicholas Rosati\nStudent Number: 1037025");
 });
